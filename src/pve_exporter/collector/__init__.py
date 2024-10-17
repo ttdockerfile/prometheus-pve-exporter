d7a7e8c915820f9cbfa7e64fe1c8f1648cfa96c5
buyfakett <buyfakett@vip.qq.com>
2024-10-17 10:36:40 +0800

    新增超时时间

--- src/pve_exporter/collector/__init__.py
+++ src/pve_exporter/collector/__init__.py
@@ -33,7 +33,7 @@ CollectorsOptions = collections.namedtuple('CollectorsOptions', [
 def collect_pve(config, host, cluster, node, options: CollectorsOptions):
     """Scrape a host and return prometheus text format for it"""
 
-    pve = ProxmoxAPI(host, **config)
+    pve = ProxmoxAPI(host, timeout=15, **config)
 
     registry = CollectorRegistry()
     if cluster and options.status:
